24b416f7b18fc68b39a8906e444c9663634f09f0
ayatigupta <ayatigupta28@gmail.com>
2026-02-23 14:51:39 +0530

    UC2 done

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,7 +1,19 @@
 public class PalindroneCheckerApp {
-    public static void main(String[] args) {
-        System.out.print("Welcome");
-        System.out.print("Modify logic to Usecase 2");
-    }
 
+    public static void main(String[] args) {
+
+        String input = "madam";  // declare string
+
+        boolean isPalindrone = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrone = false;
+                break;
+            }
+        }
+
+        System.out.println("Is it a Palindrome? : " + isPalindrone);
+    }
 }
